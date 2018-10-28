3aea840be9ae20fed11a617cbabea8c4e107140c
Bjørn Grønbæk <git@gronbaek.net>
2018-10-28 21:29:40 +0100

    gol first implementation

--- pkg/wasm_game_of_life.d.ts
+++ pkg/wasm_game_of_life.d.ts
@@ -1,3 +1,14 @@
 /* tslint:disable */
 export function greet(arg0: string): void;
 
+export enum Cell {Dead,Alive,}
+export class Universe {
+free(): void;
+
+static  new(): Universe;
+
+ render(): string;
+
+ tick(): void;
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,22 @@ cfg_if! {
     }
 }
 
+use std::fmt;
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize){
+            for &cell in line {
+                let symbol = if cell == Cell::Dead {'O'} else {'X'};
+                write!(f, "{}",symbol)?;
+            }
+            write!(f,"\n")?;
+        }
+
+        Ok(())
+    }
+}
+
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
@@ -23,22 +39,52 @@ extern {
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    let mut hello_string: String = "Hello, wasm-game-of-life!".to_owned();
+    let hello_string: String = "Hello, wasm-game-of-life!".to_owned();
     alert(&format!("{}{}",hello_string,name));
 }
 
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Cell {
     Dead = 0,
     Alive = 1
 }
 
+#[wasm_bindgen]
 pub struct Universe {
     width: u32,
     height:  u32,
     cells: Vec<Cell>
 }
 
+#[wasm_bindgen]
 impl Universe {
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }                
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
     pub fn tick(&mut self){
         let mut next = self.cells.clone();
 
@@ -48,19 +94,29 @@ impl Universe {
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row,col);
 
-                
+                let next_cell = match (cell,live_neighbors) {
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (otherwise,_) => otherwise,
+                };
+
+                next[idx] = next_cell;                
             }
         }
+
+        self.cells = next;
     }
 
     fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
 
-    fn live_neighbor_count(&self, row: u32, column: u32){
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8{
         let mut count = 0;
         for delta_row in [self.height -1,0,1].iter().cloned() {
-            for delta_column in [self.column -1,0,1].iter().cloned(){
+            for delta_column in [self.width -1,0,1].iter().cloned(){
                 if delta_column == 0 && delta_row == 0 {
                     continue;
                 }
